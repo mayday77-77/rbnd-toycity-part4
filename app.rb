56f1c3e4ea944f174605cce38840f0bededc2e23
mayday77-77 <mayday77@gmail.com>
2016-08-26 16:40:29 +0800

    Finished print all, first and last

--- app.rb
+++ app.rb
@@ -23,14 +23,14 @@ print Product.create(brand: "MySoft", name: "stuffed animal", price: 2.09)
 print Product.create(brand: "AllMy", name: "yoyo", price: 1.00)
 print Product.create(brand: "Udacity", name: "doll", price: 5.99)
 #
-# puts "-------------------"
-# puts "RETURN ALL PRODUCTS"
-# puts "-------------------"
-# print Product.all
+puts "-------------------"
+puts "RETURN ALL PRODUCTS"
+puts "-------------------"
+print Product.all
 #
-# puts "--------------------------------------------------------------"
-# puts "TEST ERROR HANDLING FOR DELETE - COMMENT OUT BEFORE SUBMITTING"
-# puts "--------------------------------------------------------------"
+#puts "--------------------------------------------------------------"
+#puts "TEST ERROR HANDLING FOR DELETE - COMMENT OUT BEFORE SUBMITTING"
+#puts "--------------------------------------------------------------"
 # print Product.destroy(1000000000000)
 #
 # puts "------------------------------------------------------------"
@@ -38,32 +38,32 @@ print Product.create(brand: "Udacity", name: "doll", price: 5.99)
 # puts "------------------------------------------------------------"
 # print Product.find(1000000000000)
 #
-# puts
-# puts "--------------------"
-# puts "RETURN FIRST PRODUCT"
-# puts "--------------------"
-# print Product.first
+puts
+puts "--------------------"
+puts "RETURN FIRST PRODUCT"
+puts "--------------------"
+print Product.first
 #
 #
-# puts
-# puts "-----------------------"
-# puts "RETURN FIRST 2 PRODUCTS"
-# puts "-----------------------"
-# print Product.first(2)
+puts
+puts "-----------------------"
+puts "RETURN FIRST 2 PRODUCTS"
+puts "-----------------------"
+print Product.first(2)
 #
-# puts
-# puts "-------------------"
-# puts "RETURN LAST PRODUCT"
-# puts "-------------------"
-# print Product.last
+puts
+puts "-------------------"
+puts "RETURN LAST PRODUCT"
+puts "-------------------"
+print Product.last
 #
-# puts
-# puts "----------------------"
-# puts "RETURN LAST 2 PRODUCTS"
-# puts "----------------------"
-# print Product.last(2)
+puts
+puts "----------------------"
+puts "RETURN LAST 2 PRODUCTS"
+puts "----------------------"
+print Product.last(2)
 #
-# puts
+puts
 # puts "-------------------------"
 # puts "DESTROY PRODUCT WITH ID 2"
 # puts "-------------------------"
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,25 +4,54 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  @@product_array = []
 
-  def self.create(attributes = nil)
+  def self.create(attributes = nil) # create new product
   	product_new = Product.new(attributes)
   	write_to_database(product_new)
   end
 
-  def self.all
-     database = File.dirname(__FILE__) + "/../data/data.csv"
-     #array = CSV.read(database)
-     CSV.foreach(database) do | row |
-     	puts row.inspect
-     end
-    	
+  def self.all # display product array
+     load_database
+     print "#{@@product_array}\n"
   end
 
-  def self.write_to_database(product)
-  	database = File.dirname(__FILE__) + "/../data/data.csv"
-  	CSV.open(database, "a+") do | csv |
-  		csv << ["1","2"]
+  def self.write_to_database(product) # save product information to CSV file
+  	database_file = File.dirname(__FILE__) + "/../data/data.csv"
+  	CSV.open(database_file, "a+") do | csv |
+  		csv << ["#{product.id}","#{product.brand}","#{product.name}","#{product.price}"]
+  	end
+  	puts product
+  end
+
+  def self.load_database # loads the CSV file into product array
+  	  database_file = File.dirname(__FILE__) + "/../data/data.csv"
+  	  # :headers option to inform that headers exist
+  	  CSV.foreach(database_file, :headers => true) do | row |
+  	  	each_product = Product.new(id: row[0], brand: row[1], name: row[2], price: row[3]) # assign values into a product class 
+  	  		@@product_array << each_product # create product array
+  	  end
+  end
+
+  def self.first(options = {})
+  	#load_database
+  	if options.to_s.to_i == 0 # if no number passed in, print first element
+  		@@product_array.first
+  	else  # else loop through product array and print the number of products accordingly
+  		new_product_array = [] 
+  		options.times { | each_time | new_product_array << @@product_array[each_time]}
+  		print new_product_array
+  	end
+  end
+
+  def self.last(options = {})
+  	#load_database
+  	if options.to_s.to_i == 0 # if no number passed in, print last element
+  		@@product_array.last
+  	else  # else loop through product array and print the number of products accordingly
+  		new_product_array = [] 
+  		options.times { | each_time | new_product_array << @@product_array.reverse[each_time]}
+  		print new_product_array.reverse
   	end
   end
 
